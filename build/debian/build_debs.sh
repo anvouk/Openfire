1ab257bc31105efb6ea18dd15f32afac6cff14a8
Stefano Terna <stefano.terna@tomorrowdata.io>
2024-01-12 17:35:13 +0100

    fix for building deb in docker

--- build/debian/build_debs.sh
+++ build/debian/build_debs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # CALL LIKE SO: bash build/debian/build_debs.sh
-# set -x
+set -x
 
 pushd .
 # This allows us to pass in the Openfire version from the command-line.
@@ -11,31 +11,32 @@ else
 fi
 export OPENFIRE_VERSION=$(echo "${OPENFIRE_FULLVERSION}" | cut -d'-' -f1)
 export DEBIAN_BUILDDATE="$(date +'%a, %d %b %Y %H:%M:%S %z')"
-export WORKDIR=tmp/debian/openfire-${OPENFIRE_VERSION}
+export WORKDIR_BOF=tmp/debian/openfire-${OPENFIRE_VERSION}
 
 if [ -d "tmp/debian" ]; then
     echo "Removing previous workdir tmp/debian"
     rm -rf tmp/debian
 fi
-mkdir -p $WORKDIR
+mkdir -p $WORKDIR_BOF
 
-cp -r distribution/target/distribution-base/. $WORKDIR/
+cp -r distribution/target/distribution-base/. $WORKDIR_BOF/
 
-mkdir -p $WORKDIR/debian
-cp build/debian/* $WORKDIR/debian/
+mkdir -p $WORKDIR_BOF/debian
+cp build/debian/* $WORKDIR_BOF/debian/
 # HACK remove out this actual script
-rm -f $WORKDIR/debian/build_debs.sh
+rm -f $WORKDIR_BOF/debian/build_debs.sh
 
 # make rules executable
-chmod 755 $WORKDIR/debian/rules
+chmod 755 $WORKDIR_BOF/debian/rules
 
-cd $WORKDIR/debian
+cd $WORKDIR_BOF/debian
 # Do some needed replacements
 sed -i.bak s/@version@/${OPENFIRE_VERSION}/g changelog
 sed -i.bak s/@builddate@/"${DEBIAN_BUILDDATE}"/g changelog
 cd ..
 # Disable build-dependencies, because they're often not useful here.
-dpkg-buildpackage -d -rfakeroot -us -uc
+#dpkg-buildpackage -d -rfakeroot -us -uc
+dpkg-buildpackage -D -us -uc
 
 pushd
 mkdir -p distribution/target/debian
